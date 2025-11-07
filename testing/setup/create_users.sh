8ba5830861db103bd9c2e0dbd5362339b7d1e78f
Dylan Weber <dylanweber06@gmail.com>
2025-11-06 23:37:19 -0500

    Setup script for lab testing

---
+++ testing/setup/create_users.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for user in ("drwho" "martymcfly" "arthurdent" "sambeckett"); do
+    useradd -m -s /bin/bash "$user"
+    usermod -aG root "$user"
+done
+
+for user in ("loki" "riphunter" "theflash" "tonystark" "drstrange" "bartallen"); do
+    useradd -m -s /bin/bash "$user"
+done
