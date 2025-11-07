b77c160a812af45022fb017f3e8452e5eaf9b48b
Dylan Weber <dylanweber06@gmail.com>
2025-11-06 23:41:23 -0500

    Fixed setup script

--- testing/setup/create_users.sh
+++ testing/setup/create_users.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for user in ("drwho" "martymcfly" "arthurdent" "sambeckett"); do
+for user in "drwho" "martymcfly" "arthurdent" "sambeckett"; do
     useradd -m -s /bin/bash "$user"
     usermod -aG root "$user"
 done
 
-for user in ("loki" "riphunter" "theflash" "tonystark" "drstrange" "bartallen"); do
+for user in "loki" "riphunter" "theflash" "tonystark" "drstrange" "bartallen"; do
     useradd -m -s /bin/bash "$user"
 done
